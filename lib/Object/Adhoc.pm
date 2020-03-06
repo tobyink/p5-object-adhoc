ddb4e7ac2058bc30776a66e99dfe834a66681639
Toby Inkster <mail@tobyinkster.co.uk>
2020-03-06 11:08:52 +0000

    bump version; changelog

--- lib/Object/Adhoc.pm
+++ lib/Object/Adhoc.pm
@@ -5,7 +5,7 @@ use warnings;
 package Object::Adhoc;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002';
+our $VERSION   = '0.003';
 
 use Hash::Util qw(lock_ref_keys);
 use Digest::MD5 qw(md5_hex);
