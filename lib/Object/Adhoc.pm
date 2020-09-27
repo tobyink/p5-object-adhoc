09d944f1de6a3be0ea9b708f42871c1df317f8fb
Toby Inkster <mail@tobyinkster.co.uk>
2020-09-27 20:08:25 +0100

    bump version; changelog

--- lib/Object/Adhoc.pm
+++ lib/Object/Adhoc.pm
@@ -5,7 +5,7 @@ use warnings;
 package Object::Adhoc;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.004';
+our $VERSION   = '0.005';
 
 use Digest::MD5 qw( md5_hex );
 use Exporter::Shiny qw( object make_class );
